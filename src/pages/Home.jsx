65613d2093059903dc41d7abd6d741666f52053a
Vinay Arya <aryavinay11112003@gmail.com>
2026-04-01 16:47:53 +0530

    updated hero image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -68,6 +68,7 @@ export default function Home() {
           {/* image + overlay + text  style={{ background: "url(./hero-image.png) no-repeat center -128px / cover", height: "41rem", width: "100%" }}*/}
           <div
             className="relative w-full h-[32vh] sm:h-[70vh] lg:h-[38rem] bg-[url('./hero-image.jpeg')] bg-cover bg-no-repeat lg:bg-[center_-144px] bg-[-94px_center]"
+            style={{ backgroundImage: 'url("./hero-image.jpeg")' }}
           >
 
             <div className="absolute lg:w-1/2 md:w-full top-0 lg:px-[4rem] px-4 py-8">
